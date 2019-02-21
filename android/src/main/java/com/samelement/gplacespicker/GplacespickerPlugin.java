04e68b025576f0a832900c537744383a6c45390e
Hanif Budiarto <m.hanifbudiarto@gmail.com>
2019-02-21 15:50:35 +0700

    migrate to AndroidX

--- android/src/main/java/com/samelement/gplacespicker/GplacespickerPlugin.java
+++ android/src/main/java/com/samelement/gplacespicker/GplacespickerPlugin.java
@@ -6,9 +6,9 @@ import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
